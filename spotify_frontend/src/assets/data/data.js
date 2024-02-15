2dc987596637ea8e54064c7035512d5d31c6750d
Rahul Singh <rathore.rahul.1999@gmail.com>
2024-02-15 23:08:49 +0530

    created separated data file

---
+++ spotify_frontend/src/assets/data/data.js
@@ -0,0 +1,37 @@
+export const homeSongData = [
+   {
+      titleText: "Focus",
+      title: "Piano",
+      description: "Relax and dead",
+      imgUrl:
+         "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+   },
+   {
+      titleText: "Tree",
+      title: "Tree",
+      description: "Relax and dead under the tree",
+      imgUrl:
+         "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630",
+   },
+   {
+      titleText: "Volcane",
+      title: "Volcano",
+      description: "Relax and dead in the Volcane",
+      imgUrl:
+         "https://media.istockphoto.com/id/670551650/photo/volcanic-mountain-in-eruption.jpg?s=612x612&w=0&k=20&c=k7EJKTpb9e1fLFuvHzB3pEIEr61xBHSCYpK7n0So2Qk=",
+   },
+   {
+      titleText: "Work",
+      title: "Work",
+      description: "Relax and dead with work",
+      imgUrl:
+         "https://st3.depositphotos.com/5295026/16266/i/450/depositphotos_162666166-stock-photo-business-man-pushing-large-stone.jpg",
+   },
+   {
+      titleText: "Diving",
+      title: "Diving",
+      description: "Relax and die via diving",
+      imgUrl:
+         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBSLldfN1y8Qsf-2XxzDQoxr_j6DuDvFkMjg&usqp=CAU",
+   },
+];
